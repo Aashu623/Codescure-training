f16ede2b3f47587469bfc41b4e09fbb315021291
Aashu623 <aashukushwah53678@gmail.com>
2024-07-29 12:38:06 +0530

    js intro comments and variables task

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// create a js file which contains variable declaration for a form which asks following basic details
+//  what is your Name, Email, age, address, phone number, gender, Are you married
+let name = 'Aashish Kushwah'
+let email = 'aashukushwah53678@gmail.com'
+let age = 22
+let address = 'Ganjbasoda, madhya pradesh'
+let phone = 7000759525
+let gender = 'Male'
+let Married = false
