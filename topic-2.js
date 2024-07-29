1813be33f0805fec490abc6c5c8e1a7d7c8e8440
Aashu623 <aashukushwah53678@gmail.com>
2024-07-29 13:07:58 +0530

    completed null undefined and objects

---
+++ topic-2.js
@@ -0,0 +1,30 @@
+// declared all the variables
+let myName, age, email, address, phone_number, gender, married;
+
+// print all the variables
+console.log(myName, age, email, address, phone_number, gender, married)
+
+// assigning them null
+myName = null
+age = null
+email = null
+address = null
+phone_number = null
+gender = null
+married = null
+
+// printing again
+console.log(myName, age, email, address, phone_number, gender, married)
+
+//declaring object name employeeDetails with key and values
+let employeeDetails = {
+    name: myName,
+    age: age,
+    email: email,
+    address: address,
+    phone_number: phone_number,
+    gender: gender,
+    married: married
+}
+
+console.log(employeeDetails)
